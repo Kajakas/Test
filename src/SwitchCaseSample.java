cd50fb363bad73c500e8d6ccf11ca76a6b52edbb
Kajakas <rickker@gmail.com>
2017-10-04 11:38:45 +0300

    asd

--- src/SwitchCaseSample.java
+++ src/SwitchCaseSample.java
@@ -1,9 +1,13 @@
+import java.util.*;
+
 public class SwitchCaseSample {
 	public static void main(String[] args) {
-		System.out.printf(valueGrade(5));
+		System.out.printf(valueGradeSC(5));
+		System.out.printf("%n");
+		System.out.printf(valueGradeMap(1));
 	}
 	
-	public static String valueGrade(int a) {
+	public static String valueGradeSC(int a) {
         String gradestr;
 		switch (a) {
 	        case 1:  gradestr = "Debil blyat!";
@@ -30,5 +34,22 @@ public class SwitchCaseSample {
 	                 break;
 		}
 		return gradestr;
+	}
+	
+	public static String valueGradeMap(int a) {
+		HashMap<Integer, String> grade = new HashMap<Integer, String>();
+		grade.put(1, "Debil blyat!");
+		grade.put(2, "Väga loll oled, miks sa üldse vaevud?");
+		grade.put(3, "Väga sitasti läks!");
+		grade.put(4, "Halvasti läks!");
+		grade.put(5, "Harju keskmine!");
+		grade.put(6, "Hakkab juba minema!");
+		grade.put(7, "Wat");
+		grade.put(8, "Oled nüüd uhke w?");
+		grade.put(9, "You can squat like true slav!");
+		grade.put(10, "Sisesta hinne vahemikus 1-10");
+	
+		return (grade.containsKey(a)) ? grade.get(a) : "Sisesta hinne vahemikus 1-10!";
+
 	}
 }
